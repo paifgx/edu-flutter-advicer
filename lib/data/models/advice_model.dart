8b9009a7d4ad360743b34178e00ea72943ad70d8
Patrik Garten <info@garten.dev>
2025-12-10 11:24:57 +0100

    fix 2

--- lib/data/models/advice_model.dart
+++ lib/data/models/advice_model.dart
@@ -4,9 +4,6 @@ class AdviceModel extends AdviceEntity {
   const AdviceModel({required super.advice, required super.id});
 
   factory AdviceModel.fromJson(Map<String, dynamic> json) {
-    return AdviceModel(
-      advice: json['slip']['content_text'],
-      id: json['slip']['id'],
-    );
+    return AdviceModel(advice: json['slip']['advice'], id: json['slip']['id']);
   }
 }
